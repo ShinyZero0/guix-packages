6c7097ae66836c2875764f93ec3cbd5a3b4e6ef8
zero@fedora <shinyzero0@tilde.club>
2023-09-21 22:39:56 +0300

    zerolib: update

--- zero/packages/guile-zerolib.scm
+++ zero/packages/guile-zerolib.scm
@@ -14,10 +14,10 @@
        (method git-fetch)
        (uri (git-reference
               (url "https://codeberg.org/shinyzero0/zerolib.scm.git")
-              (commit "bc4c38d6eec5c4dc23e78c42566ea63a71e99854")))
+              (commit "1c12e135d44dc105e3c0de29df106016a154da77")))
        (sha256
          (base32
-           "0688rgn1qylkyvm7kp76l3l8rha8rny4gz2vcc6qv7nwl74x6alm"))))
+           "0m5zz8a8pnw69ffprrdgj84fmssxpdhsb94ial01xnxxxrflcs01"))))
    (build-system guile-build-system)
    (inputs
      (list
